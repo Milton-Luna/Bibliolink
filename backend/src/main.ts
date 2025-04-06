a6312d42d6c3c5bc6516e74dbf602dcf89dc5d5b
Marlon Moncayo <marlonmoncayo7474@gmail.com>
2025-04-06 18:10:40 -0500

    Subiendo cambios del Puerto: Actualizacion en app.module y main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { SERVER_PORT } from 'config/constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const configService = app.get(ConfigService);
+  // server port
+  const port = +(configService.get<number>(SERVER_PORT) ?? 3000);
+  await app.listen(port);
+  console.log(`Server is running on http://localhost:${port}`);
 }
 bootstrap();
