570a394811c76c8cc3fbaea46832869a5e854ed0
Marlon Moncayo <marlonmoncayo7474@gmail.com>
2025-04-07 16:18:05 -0500

    app.module importacion de TypeOrmModule sobre conectividad de Base de Datos y actualizacion de contenido sobre constants.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from 'config/constants';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get<string>(DB_HOST),
+        port: +(configService.get<number>(DB_PORT) ?? 3306),
+        username: configService.get<string>(DB_USER),
+        password: configService.get<string>(DB_PASSWORD),
+        database: configService.get<string>(DB_DATABASE),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        logging: false
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
